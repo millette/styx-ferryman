fc3d6a7f343aa592ed61dff276325f897bb091e5
Robin Millette <robin@millette.info>
2017-04-24 20:50:58 -0400

    Link to majestic million.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict'
 
+// 78 MiB
+// multiple columns
+// GlobalRank,TldRank,Domain,TLD,RefSubNets,RefIPs,IDN_Domain,IDN_TLD,PrevGlobalRank,PrevTldRank,PrevRefSubNets,PrevRefIPs
+// http://downloads.majestic.com/majestic_million.csv
+
 const data = require('./top-1m')
 
 console.log(data.length)
