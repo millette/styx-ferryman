61476a156f671df96358760c5f1a481e0075d504
Robin Millette <robin@millette.info>
2017-04-30 19:00:13 -0400

    Make task optional, default to nop.

--- src/f2.js
+++ src/f2.js
@@ -7,34 +7,12 @@ const miss = require('mississippi')
 const tb = require('through-batch')
 const parallel = require('parallel-transform')
 
-/*
-const yo = (item, cb) => setTimeout((i) => {
-  // console.log('.')
-  cb(null, i)
-}, 100, item)
+const nop = (item, cb) => cb(null, item)
 
-const out = miss.through.obj((chunk, enc, cb) => {
-  cb(null, JSON.stringify(chunk) + '\n')
-})
-*/
-
-const work = (u, task, per) => miss.pipe(
+module.exports = (u, task, per) => miss.pipe(
   hyperquest(u),
   JSONStream.parse('rows.*'),
-  parallel(per || 5, task),
+  parallel(per || 5, task || nop),
   tb(per || 5)
 )
 
-/*
-miss.pipe(
-  work(
-    'http://localhost:5993/u2/_design/timeout/_view/timeout?reduce=false',
-    yo,
-    5
-  ),
-  out,
-  process.stdout
-)
-*/
-
-module.exports = work
