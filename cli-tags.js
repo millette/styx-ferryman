e1701bfe7ab9522464f3391781f7ac4f15f2ffa7
Robin Millette <robin@millette.info>
2017-05-17 17:01:50 -0400

    Output most popular tags.

---
+++ cli-tags.js
@@ -0,0 +1,30 @@
+'ust strict'
+
+// npm
+const hyperquest = require('hyperquest')
+
+const u = 'http://localhost:5993/u2/_design/categories/_view/categories?group_level=1'
+const ff = (a) => a.value > 3
+const mm = (a) => a.key[0]
+let str = ''
+
+const ss = (a, b) => {
+  const ka = a.value
+  const kb = b.value
+  if (ka > kb) { return 1 }
+  if (ka < kb) { return -1 }
+  return 0
+}
+
+hyperquest(u)
+  .on('data', (chunk) => str += chunk)
+  .on('end', () => {
+    const rep = JSON.parse(str)
+    const tags = rep.rows
+      .sort(ss)
+      .reverse()
+      .filter(ff)
+      .map(mm)
+      .sort()
+    console.log(JSON.stringify(tags, null, ' '))
+  })
